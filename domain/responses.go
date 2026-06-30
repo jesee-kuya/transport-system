23abbf42805d8d95539d4d9d646c8954918a8f90
jesee kuya <kuyajesee@proton.me>
2026-06-30 03:34:35 +0300

    domain: add LoginResponse and ForgotPasswordResponse types

--- domain/responses.go
+++ domain/responses.go
@@ -8,3 +8,16 @@ type SignUpResponse struct {
 	Email    string    `json:"email"`
 	Role     string    `json:"role"`
 }
+
+type LoginResponse struct {
+	Token    string    `json:"token"`
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	Role     string    `json:"role"`
+}
+
+type ForgotPasswordResponse struct {
+	ResetToken string `json:"reset_token"`
+	Message    string `json:"message"`
+}
