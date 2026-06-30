17c24c058b9802ca5d38db8a7edec23b766a0f9b
jesee kuya <kuyajesee@proton.me>
2026-06-30 03:34:53 +0300

    domain: add Trip and PrivateTrip types

---
+++ domain/trip.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Trip struct {
+	ID        uuid.UUID  `db:"id" json:"id"`
+	SchoolID  uuid.UUID  `db:"school_id" json:"school_id"`
+	DriverID  uuid.UUID  `db:"driver_id" json:"driver_id"`
+	BusID     uuid.UUID  `db:"bus_id" json:"bus_id"`
+	TripType  string     `db:"trip_type" json:"trip_type"`
+	Status    string     `db:"status" json:"status"`
+	StartedAt *time.Time `db:"started_at" json:"started_at"`
+	EndedAt   *time.Time `db:"ended_at" json:"ended_at"`
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
+}
+
+type PrivateTrip struct {
+	ID        uuid.UUID  `db:"id" json:"id"`
+	DriverID  uuid.UUID  `db:"driver_id" json:"driver_id"`
+	MatchID   uuid.UUID  `db:"match_id" json:"match_id"`
+	Status    string     `db:"status" json:"status"`
+	StartedAt *time.Time `db:"started_at" json:"started_at"`
+	EndedAt   *time.Time `db:"ended_at" json:"ended_at"`
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
+}
