2a842f8d79953c65dc1f0d06fdd23cf409ecb29a
jesee kuya <kuyajesee@proton.me>
2026-06-30 03:35:59 +0300

    middleware: accept JWTConfig in NewMiddleware so auth middleware can verify tokens

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,5 +1,7 @@
 package middleware
 
-func NewMiddleware() Middleware {
-	return &MiddlewareStruct{}
+import "github.com/jesee-kuya/transport-system/domain"
+
+func NewMiddleware(cfg *domain.JWTConfig) Middleware {
+	return &MiddlewareStruct{JWT: cfg}
 }
