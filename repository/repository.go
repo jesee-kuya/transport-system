134c8556f2d05506bce0a9c68e6e5ec99fcbe0a1
jesee kuya <kuyajesee@proton.me>
2026-06-30 03:35:21 +0300

    repository: add NewSchoolRepository constructor

--- repository/repository.go
+++ repository/repository.go
@@ -5,3 +5,7 @@ import "github.com/jmoiron/sqlx"
 func NewAuthRepository(db *sqlx.DB) AuthRepository {
 	return &AuthRepositoryStruct{DB: db}
 }
+
+func NewSchoolRepository(db *sqlx.DB) SchoolRepository {
+	return &SchoolRepositoryStruct{DB: db}
+}
