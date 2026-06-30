894b7fdae53ea1301fea1245688a83e9b9fb9155
jesee kuya <kuyajesee@proton.me>
2026-06-30 03:35:31 +0300

    service: wire JWTConfig into NewAuthService and add NewSchoolService constructor

--- service/service.go
+++ service/service.go
@@ -1,7 +1,14 @@
 package service
 
-import "github.com/jesee-kuya/transport-system/repository"
+import (
+	"github.com/jesee-kuya/transport-system/domain"
+	"github.com/jesee-kuya/transport-system/repository"
+)
 
-func NewAuthService(repo repository.AuthRepository) Authentication {
-	return &AuthService{AuthRepo: repo}
+func NewAuthService(repo repository.AuthRepository, cfg *domain.JWTConfig) Authentication {
+	return &AuthService{AuthRepo: repo, JWTConfig: cfg}
+}
+
+func NewSchoolService(repo repository.SchoolRepository) SchoolService {
+	return &SchoolSvc{SchoolRepo: repo}
 }
